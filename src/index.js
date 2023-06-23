4a6c33b98baca789a1caa30e48249a32c7943ddd
Cooper <cmphill@pm.me>
2023-06-23 16:00:08 -0600

    added the updated fetch request

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 
 let recipeSearch = 'taco'
-fetch(`https://api-ninjas.com/api/recipe?query=${recipeSearch}&key=${keyString}`)
+fetch(`https://api-ninjas.com/api/recipe?query=${recipeSearch}`, {
+    headers: {
+        'X-Api-Key': apiKey
+    },
+    method: 'GET',
+})
     .then(res =>res.json())
     .then(data => console.log(data))
+    .catch(error => console.error('Error: ', error))
 
 

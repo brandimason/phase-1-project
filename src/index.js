416c849d26750cc3631c921da2f74268cf2b1006
Cooper <cmphill@pm.me>
2023-06-26 09:59:18 -0600

    wrote the new fetch and adding variable assignments for key data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 
-// let recipeSearch = 'taco'
-fetch(`http://localhost:3000/recipe`)
-.then(res =>res.json())
-.then(data => console.log(data))
-.catch(e => console.log(e))
+let recipe = 'bread'
+fetch (`https://www.themealdb.com/api/json/v1/1/search.php?s=${recipe}`)
+    .then(res => res.json())
+    .then (data => init(data))
+
+function init(recipeData) {
+    recipeName = (recipeData.meals[0].strMeal)
+
+}
 
 
 

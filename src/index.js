e27014220850ec28085a6968a3729760cb3453f1
Brandi L Mason <masonbrandi66@gmail.com>
2023-06-27 09:00:03 -0600

    chore: before cooper merge

--- src/index.js
+++ src/index.js
@@ -5,5 +5,10 @@ fetch(`http://localhost:3000/recipe`)
 .then(data => console.log(data))
 .catch(e => console.log(e))
 
+// search for receipes
+// show the recipe ingredients 
+// show the recipe instructions
 
+// show your favorite reipes at the bottom
+// add your own recipes (using a form)
 
